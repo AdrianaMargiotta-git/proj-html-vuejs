ae1a05b721c7446bfee6508e00b4d61bae8e87f1
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-12-09 17:00:56 +0100

    completato l'header

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+    el: "#app",
+    data: {
+        titolo: "Max",
+        titolo2: "Coach",
+        linksheader: [
+            'Home',
+            'Pages',
+            'Courses',
+            'Features',
+            'Blog',
+            'Shop'
+        ]
+    }
+})
