b72d2ee71247c3bf2cca4e3b02755fc10dc19366
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2020-12-10 10:40:48 +0100

    sezione1 e sezione 4 da completare

--- js/script.js
+++ js/script.js
@@ -10,6 +10,36 @@ var app = new Vue ({
             'Features',
             'Blog',
             'Shop'
+        ],
+        linkCourses:[
+            {
+                image: 'course-02-480x298.jpg',
+                price: '$40.00',
+                title: 'Learning to Write as a Professional Author',
+                lessons: '20 lessons',
+                students: '50 students'
+            },
+            {
+                image: 'stock-full-hd-03-480x298.jpg',
+                price: 'Free',
+                title: 'Customer-centric Info-Tech Strategies',
+                lessons: '24 lessons',
+                students: '769 students'
+            },
+            {
+                image: 'course-02-480x298.jpg',
+                price: '$40.00',
+                title: 'Learning to Write as a Professional Author',
+                lessons: '20 lessons',
+                students: '50 students'
+            },
+            {
+                image: 'stock-full-hd-03-480x298.jpg',
+                price: 'Free',
+                title: 'Customer-centric Info-Tech Strategies',
+                lessons: '24 lessons',
+                students: '769 students'
+            }
         ]
     }
 })
